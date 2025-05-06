df92d6dfbc885f1431948ab6cb5cc343878357c9
AdrianBustanil <rianbustanil450@gmail.com>
2025-05-06 16:10:11 +0700

    menambahkan set id

--- frienClassSatu.cpp
+++ frienClassSatu.cpp
@@ -11,3 +11,32 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa 
+{
+    private:
+        int id;
+
+public:
+    void setid(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a);
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+}
